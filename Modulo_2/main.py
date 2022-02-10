cdad1d3566e1371d7d83abf549362f6aa50d3048
Douglas Littig <gittil@protonmail.com>
2022-02-10 15:33:37 -0300

    commitando o módulo 2

---
+++ Modulo_2/main.py
@@ -0,0 +1,7 @@
+import operacoes as op
+
+op.somar(2, 4)
+op.subtrair(4,2)
+op.dividir(4,2)
+op.multiplicar(2,4)
+op.imprimir("Uma mensagem qualquer...")
---
+++ Modulo_2/operacoes.py
@@ -0,0 +1,16 @@
+"""
+Fornece operações básicas matemáticas como adição, subtração, divisão e multiplicação. Também imprime variáveis passadas por argumentos.
+"""
+def somar(a, b):
+    print(a + b)
+
+def subtrair(a, b):
+    print(a - b)
+
+def dividir(a, b):
+    print(a / b)
+
+def multiplicar(a, b):
+    print(a * b)
+def imprimir(val):
+    print(val)
